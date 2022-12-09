65928fe0ab5dc6bfac168a9359af71902d3ace1b
Vadim <vadims.trojanovs@gmail.com>
2022-12-09 16:21:38 +0200

    1 и 3 задание

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int num = number.Length;
+if (num == 5)
+
+{
+  if (number[0]==number[4]){
+    Console.WriteLine("Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine("Ваше число: {number} - НЕ палиндром.");
+}
+else Console.WriteLine("Введенно неверное число");
